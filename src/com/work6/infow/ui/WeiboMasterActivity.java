db4d38e10edc8c9d90fd0bd25c6651c58b27a91b
zzr <zzr@163.com>
2012-04-10 22:06:08 +0800

    zzr commit

---
+++ src/com/work6/infow/ui/WeiboMasterActivity.java
@@ -0,0 +1,13 @@
+package com.work6.infow.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeiboMasterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
